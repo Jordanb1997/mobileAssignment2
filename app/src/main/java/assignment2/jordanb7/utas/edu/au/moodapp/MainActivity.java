bbdb316249a13bde4758d3c3e84078cdf3a03520
Jordan Bannister <40512499+Dragoneer122@users.noreply.github.com>
2019-05-07 10:48:21 +1000

    adding project
    file for the android studio project

---
+++ app/src/main/java/assignment2/jordanb7/utas/edu/au/moodapp/MainActivity.java
@@ -0,0 +1,13 @@
+package assignment2.jordanb7.utas.edu.au.moodapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
