7eee18bba2f1cd7bfd9f3c8458605742895dd33d
Jordan <40512499+Dragoneer122@users.noreply.github.com>
2019-05-23 14:10:02 +1000

    database stuff

---
+++ app/src/main/java/assignment2/jordanb7/utas/edu/au/moodapp/EntryTable.java
@@ -0,0 +1,22 @@
+package assignment2.jordanb7.utas.edu.au.moodapp;
+
+public class EntryTable {
+    public static final String TABLE_NAME = "entries";
+    public static final String KEY_ENTRY_ID = "entry_id";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_DATE = "date";
+    public static final String KEY_MOOD = "mood";
+    public static final String KEY_TEXT = "text";
+    public static final String KEY_IMAGE = "image";
+
+    public static final String CREATE_STATEMENT = "CREATE TABLE "
+            + TABLE_NAME
+            + " (" + KEY_ENTRY_ID + " integer primary key autoincrement, "
+            + KEY_TITLE + " string not null, "
+            + KEY_DATE + " int not null, "
+            + KEY_MOOD + " string not null "
+            + KEY_TEXT + " string not null "
+            + KEY_IMAGE + " string not null "
+            +");";
+
+}
