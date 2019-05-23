b85d957dfdb8f757c17423afef8c4e30a60f9976
Jordan <40512499+Dragoneer122@users.noreply.github.com>
2019-05-23 14:48:05 +1000

    database stuff, foxed start up crash

---
+++ app/src/main/java/assignment2/jordanb7/utas/edu/au/moodapp/Entry.java
@@ -0,0 +1,28 @@
+package assignment2.jordanb7.utas.edu.au.moodapp;
+
+public class Entry {
+    private int mEntryID;
+    private String mTitle;
+    private int mDate;
+    private String mMood;
+    private String mText;
+    private String mImage;
+
+    public int getmEntryID() { return mEntryID; }
+    public void setmEntryID(int s) { this.mEntryID = s; }
+
+    public String getmTitle() { return mTitle; }
+    public void setmTitle(String s) { this.mTitle= s; }
+
+    public int getmDate() { return mDate; }
+    public void setmDate(int s) { this.mDate= s; }
+
+    public String getmMood() { return mMood; }
+    public void setmMood(String s) { this.mMood= s; }
+
+    public String getmText() { return mText; }
+    public void setmText(String s) { this.mText= s; }
+
+    public String getmImage() { return mImage; }
+    public void setmImage(String s) { this.mImage= s; }
+}
