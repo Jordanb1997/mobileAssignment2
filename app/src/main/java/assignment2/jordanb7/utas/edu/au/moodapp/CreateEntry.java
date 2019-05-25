38b74aa97e66e589e8a3484db261b6906b7e4ef0
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2019-05-25 11:36:16 +1000

    date can be displayed

--- app/src/main/java/assignment2/jordanb7/utas/edu/au/moodapp/CreateEntry.java
+++ app/src/main/java/assignment2/jordanb7/utas/edu/au/moodapp/CreateEntry.java
@@ -1,23 +1,31 @@
 package assignment2.jordanb7.utas.edu.au.moodapp;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 
+import java.util.Calendar;
+
 public class CreateEntry extends AppCompatActivity {
+    Calendar cal;
+    DatePickerDialog date;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_entry);
 
-        Button datePicker = findViewById(R.id.date_picker_btn);
 
         Spinner spinner = (Spinner) findViewById(R.id.mood);
-
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.mood_items, android.R.layout.simple_spinner_item);
@@ -25,6 +33,34 @@ public class CreateEntry extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+
+
+
+
+
+        final Button datePicker = findViewById(R.id.date_picker_btn);
+        datePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cal = Calendar.getInstance();
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                int month = cal.get(Calendar.MONTH);
+                int year = cal.get(Calendar.YEAR);
+
+                date = new DatePickerDialog(CreateEntry.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+                    }
+                },day,month,year);
+                date.show();
+            }
+        });
+
+
+
+
 
 
     }
