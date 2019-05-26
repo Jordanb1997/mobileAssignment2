740a5006de04f4811e4b16406487218c7bac8f6a
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2019-05-26 14:52:00 +1000

    Maybe entry working

--- app/src/main/java/assignment2/jordanb7/utas/edu/au/moodapp/CreateEntry.java
+++ app/src/main/java/assignment2/jordanb7/utas/edu/au/moodapp/CreateEntry.java
@@ -2,22 +2,24 @@ package assignment2.jordanb7.utas.edu.au.moodapp;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class CreateEntry extends AppCompatActivity {
     Calendar cal;
     DatePickerDialog date;
-
-
-
+    int i;
 
 
     @Override
@@ -25,13 +27,28 @@ public class CreateEntry extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_entry);
 
+        Database databaseConnection = new Database(this);
+        final SQLiteDatabase db = databaseConnection.open();
 
+        final ArrayList<Entry> entries = EntryTable.selectAll(db);
+
+        final TextView title = findViewById(R.id.journal_title_input);
 
 
         Button fin = findViewById(R.id.journal_done_btn);
         fin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                final Entry p = entries.get(i);
+
+                p.setTitle(title.getText().toString());
+                EntryTable.insert(db,p);
+                Log.d("sam", title.getText().toString());
+
+
+
                 Intent m = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(m);
             }
