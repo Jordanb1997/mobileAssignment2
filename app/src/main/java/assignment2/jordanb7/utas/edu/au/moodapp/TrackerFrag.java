3c655b48799b5c9bba2c1045f2037d37dee99be1
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2019-05-26 17:42:57 +1000

    tacker list, WIP

--- app/src/main/java/assignment2/jordanb7/utas/edu/au/moodapp/TrackerFrag.java
+++ app/src/main/java/assignment2/jordanb7/utas/edu/au/moodapp/TrackerFrag.java
@@ -1,15 +1,32 @@
 package assignment2.jordanb7.utas.edu.au.moodapp;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TrackerFrag extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_tracker, container, false);
+        View f = inflater.inflate(R.layout.frag_entries,container,false);
+
+        Database databaseConnection = new Database(getActivity());
+        final SQLiteDatabase db = databaseConnection.open();
+
+        final ArrayList<Entry> entries = EntryTable.selectAll(db);
+
+
+        ListView entriesList = f.findViewById(R.id.entriesList);
+        EntryAdapter EntryListAdapter = new EntryAdapter(f.getContext(),android.R.layout.simple_list_item_1,entries);
+        entriesList.setAdapter(EntryListAdapter);
+
+        return f;
     }
 }
